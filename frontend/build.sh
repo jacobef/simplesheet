c8af078bffc95c54a23ee1290d0cbc8b2dccf25d
jacobef <jacobef2@gmail.com>
2025-04-02 23:52:26 -0600

    display error message if pkg-config can't find raylib

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,8 @@
+if ! pkg-config --exists raylib; then
+    echo "Raylib not found. Install it with 'brew install raylib'."
+    exit 1
+fi
+
 clang grid.c zoom.m -o simplesheet \
     $(pkg-config --cflags --libs raylib) \
     -framework Cocoa -framework CoreVideo -framework IOKit -framework OpenGL
